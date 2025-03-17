f5a9a7d35aa6b0a3003017d2fd5652d76282fd10
Q-digital <no-reply@q-digital.org>
2025-03-17 16:45:31 +0400

    login to service

--- user_service/src/Controller/Api/Auth/SignInController.php
+++ user_service/src/Controller/Api/Auth/SignInController.php
@@ -3,36 +3,23 @@
 namespace App\Controller\Api\Auth;
 
 use App\ApiResource\TokenResource;
-use App\Entity\User;
 use App\DTO\SignInDTO;
-use Doctrine\ORM\EntityManagerInterface;
-use Lexik\Bundle\JWTAuthenticationBundle\Services\JWTTokenManagerInterface;
+use App\Service\UserService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpKernel\Attribute\MapRequestPayload;
-use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Attribute\Route;
 
 class SignInController extends AbstractController
 {
+    public function __construct(
+        private readonly UserService $userService
+    ) {
+    }
+
     #[Route('/api/auth/signin', name: 'api_login', methods: ['POST'])]
     public function execute(
         #[MapRequestPayload] SignInDTO $request,
-        EntityManagerInterface $entityManager,
-        UserPasswordHasherInterface $passwordHasher,
-        JWTTokenManagerInterface $JWTManager
     ): TokenResource {
-        $user = $entityManager->getRepository(User::class)->findOneBy(['email' => $request->email]);
-
-        if (is_null($user)) {
-            throw $this->createNotFoundException('Пользователь не найден');
-        }
-
-        if (!$passwordHasher->isPasswordValid($user, $request->password)) {
-            throw $this->createNotFoundException('Пользователь не найден');
-        }
-
-        $token = $JWTManager->create($user);
-
-        return new TokenResource($token);
+        return $this->userService->login($request);
     }
 }
--- user_service/src/Service/UserService.php
+++ user_service/src/Service/UserService.php
@@ -3,10 +3,14 @@
 namespace App\Service;
 
 use App\ApiResource\TokenResource;
+use App\DTO\SignInDTO;
 use App\DTO\SignUpDTO;
+use App\Entity\User;
 use App\Event\RegisterEvent;
 use App\Repository\UserRepository;
 use Lexik\Bundle\JWTAuthenticationBundle\Services\JWTTokenManagerInterface;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Contracts\EventDispatcher\EventDispatcherInterface;
 
 class UserService
@@ -14,12 +18,30 @@ class UserService
     public function __construct(
         private readonly JWTTokenManagerInterface $JWTTokenManager,
         private readonly EventDispatcherInterface $eventDispatcher,
-        private readonly UserRepository $userRepository
+        private readonly UserRepository $userRepository,
+        private readonly UserPasswordHasherInterface $passwordHasher,
     ) {
 
     }
 
-    public function register(SignUpDTO $request)
+    public function login(SignInDTO $request): TokenResource
+    {
+        $user = $this->userRepository->findByEmail($request->email);
+
+        if (is_null($user)) {
+            $this->notFoundUserException();
+        }
+
+        if (!$this->passwordHasher->isPasswordValid($user, $request->password)) {
+            $this->notFoundUserException();
+        }
+
+        $token = $this->JWTTokenManager->create($user);
+
+        return new TokenResource($token);
+    }
+
+    public function register(SignUpDTO $request): TokenResource
     {
         $user = $this->userRepository->register($request->email, $request->password);
         $token = $this->JWTTokenManager->create($user);
@@ -28,5 +50,10 @@ class UserService
         $this->eventDispatcher->dispatch($event, RegisterEvent::NAME);
 
         return new TokenResource($token);
+    }
+
+    private function notFoundUserException()
+    {
+        throw new NotFoundHttpException('Пользователь не найден');
     }
 }
